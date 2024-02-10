7ddb6b5247dd6076a230fa7e1493f86e80caba9a
juan.gonzalezp <jdgonzal2@gmail.com>
2024-02-10 18:39:24 -0500

    feat: Selenium con Java y Cucumber, Página Base.
    - Borramos 2 líneas de "FreeRangeNavegation.feature".
    - Creamos "BasePage.java".
    
    #step_20

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasePage {
+    // Declaración de una variable estática 'driver' de tipo WebDriver
+    // Esta variable va a ser compartida por todas las instancias de BasePage y sus subclases
+    protected static WebDriver driver;
+    
+    // Declaración de una variable de instancia 'wait' de tipo WebDriverWait.
+    // Se inicializa inmediatamente con una instancia dew WebDriverWait utilizando el 'driver' estático
+    // WebDriverWait se usa para poner esperas explícitas en los elementos web
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    // Configura el WebDriver para Chrome usando WebDriverManager.
+    // WebDriverManager va a estar descargando y configurando automáticamente el driver del navegador
+    static {
+        WebDriverManager.chromedriver().setup();
+        // Inicializa la variable estática 'driver' con una instancia de ChromeDriver
+        driver = new ChromeDriver();
+    }
+
+    // Este es el constructor de BasePage que acepta un objeto WebDriver como argumento.
+    public BasePage(WebDriver driver){
+        BasePage.driver = driver;
+    }
+
+    // Método estático para navegar a una URL.
+    public static void  navigateTo( String url){
+        driver.get(url);
+    }
+    
+}
--- src/test/resources/FreeRangeNavegation.feature
+++ src/test/resources/FreeRangeNavegation.feature
@@ -5,5 +5,3 @@ Feature: Navigation bar
  
 Scenario: I can access the subpages through the navigation bar 
     Given I navigate to www.freerangetesters.com 
-    When I try to access the free sections through the navigation bar 
-    Then I am redirected to the right page
