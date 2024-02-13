9b29b2d075234d97e47c9399d373c1e573f786da
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-02-13 17:09:40 -0500

    feat: Selenium con Java y Cucumber, Cerra el Browser. (#17)
    - Añadimos en "BasePage.java" , un método para cerrar el browser llamado `CloseBrowser`.
    - Añadimos en "TestRunner.java" un @AfterClass que llama el `CloseBrowser`.
    
    #step_27

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,9 +1,11 @@
 package runner;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import pages.BasePage;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -13,6 +15,9 @@ import io.cucumber.junit.CucumberOptions;
 )   
 
 public class TestRunner {
-    
+@AfterClass
+    public static void cleanDriver(){
+        BasePage.closeBrowser();
+    }
     
 }
