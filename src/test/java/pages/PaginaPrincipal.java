6cef4eff1d5279fecdae8efb8c86d84dd5e2142a
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2024-02-13 16:26:48 -0500

    feat: Selenium con Java y Cucumber, *BasePage.java* Columna vertebral (#16)
    - Añadimos un `WebElement` llamado `Find` en "BasePage.java" , con la espera de `wait`.
    - Añadimos un `clickElement` de uso público para hacer click en el `WebElement` en "BasePage.java".
    - Añadimos una variable `searchButton` en `PaginaPrincipal.java` con el valor del xpath Relativo.
    - Añadimos el uso del `clickElement` con el valor de `searchButton` en `PaginaPrincipal.java`.
    
    #step_25

--- src/test/java/pages/PaginaPrincipal.java
+++ src/test/java/pages/PaginaPrincipal.java
@@ -1,6 +1,9 @@
 package pages;
 
 public class PaginaPrincipal extends BasePage{
+
+    // Definimos searchButton como un xpath del botón "Ver curso" de "selenium-y-cucumber-java"
+    private String searchButton = "//a[@href='/selenium-y-cucumber-java'][normalize-space()='Ver curso']";
     
     public PaginaPrincipal(){
         // Se llama el Constructor Padre
@@ -9,6 +12,9 @@ public class PaginaPrincipal extends BasePage{
 
     // Método para navegar a `www.freerangetesters.com`
     public void navigateToFreeRangeTesters(){
+        // Sitio o URL a nevegar
         navigateTo("https://www.freerangetesters.com");
+        // Damos un click sobre el
+        clickElement(searchButton);
     }
 }
