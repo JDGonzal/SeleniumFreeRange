71fe07b11ff44476ce4378dbb052eff09de8e7b7
juan.gonzalezp <jdgonzal2@gmail.com>
2024-02-15 21:00:28 -0500

    feat: Selenium con Java y Cucumber, Solo Cursos y otro mas.
    - Se comentan valores de `Scenario` en 'FreeNavigations.feature'.
    - Añadimos un `Scenario` en 'FreeNavigations.feature' para Cursos y otra opción.
    - Añadimos en 'FreeRangeSteps.java' el uso del `@And`.
    - Creamos los archivos 'PaginaCursos.java' y 'PaginaFundamentosTesting'.
    
    #step_35

--- src/test/java/steps/FreeRangeSteps.java
+++ src/test/java/steps/FreeRangeSteps.java
@@ -1,14 +1,21 @@
 package steps;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 // Importación sugerida para traer todo de `cucumber.java`
 // import io.cucumber.java.en.*
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import pages.PaginaCursos;
+import pages.PaginaFundamentosTesting;
+
 import pages.PaginaPrincipal;
 
 public class FreeRangeSteps {
 
     PaginaPrincipal landingPage = new PaginaPrincipal();
+    PaginaCursos cursosPage = new PaginaCursos();
+    PaginaFundamentosTesting fundamentosPage = new PaginaFundamentosTesting();
+
 
     @Given ("I navigate to www.freerangetesters.com")
     public void iNavigateToFRT(){
@@ -18,5 +25,11 @@ public class FreeRangeSteps {
     @When ("I go to {word} using the navigation bar")
     public void navigationBarUse(String section){
         landingPage.clickOnSectionNavigationBar(section);
+    }
+
+    @And ("select Introduction to Testing")
+    public void navigateToIntro(){
+        cursosPage.clickFundamentosTestingLink();
+        fundamentosPage.clickIntroduccionTestingLink();
     }
 }
