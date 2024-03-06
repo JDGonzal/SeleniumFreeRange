45ec67da2347ee56b9e41817333775e5bf9cc807
juan.gonzalezp <jdgonzal2@gmail.com>
2024-03-06 18:12:48 -0500

    feat: Selenium con Java y Cucumber, Primer Click.
    - En "BasePage.java" metodo privado llamado `Find`.
    - En "BasePage.java" metodo público usa `Find` para hacer click.
    - En "GooglePage.java" variable privada que es un locator del botón.
    - En "GooglePage.java" método que hace click en el botón.
    - En "GoogleSteps.java" el llamado al métdo nuevo para hacer click.
    
    #step_82

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -2,13 +2,22 @@ package pages;
 
 
 public class GooglePage extends BasePage {
+  // el locator del botón de Search
+  private String searchButton = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']";
+
   // Usamos el Contructor del padre
   public GooglePage(){
     super(driver);
   }
+
   // Vamos a relizar la Navegación
   public void navigateToGoogle(){
     navigateTo("https://www.google.com");
+  }
+
+  // Hacemos un click en el botón de Search
+  public void clickGoogleSearch(){
+    clickElement(searchButton);
   }
   
 }
